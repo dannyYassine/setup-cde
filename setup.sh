a074702f90bf3c560d2fd4a03039d7400fb705c4
“Danny <dannyyassine@gmail.com>
2024-02-17 17:32:56 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-# install vscode server
+## Install vscode server
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
-
 tar -xf vscode_cli.tar.gz
 
-#./code tunnel
-#Or as a background service 
-/code tunnel service install 
+## Run code server inline or background
 
+# Inline
+#./code tunnel
+
+# Background
+./code tunnel service install
+# Monitor service
+./code tunnel service log
+
+## Set git info
 git config --global user.email “<your_email>”
 git config --global user.name “<your username>”
 
+## Set writable permissions
 sudo chmod -R 777 .
 
-Sudo apt-get update
-
+## Install dependencies
+sudo apt-get update
 Sudo apt-get install make
 Sudo apt-get install git -y
 
-# install docker https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-debian-10
+## Install docker https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-debian-10
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -

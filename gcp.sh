ad708f66e659db9d6cc500e9b55382711acce51a
Danny Yassine <dannyyassine@gmail.com>
2024-02-18 12:17:37 -0500

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+### 
+# ASSUMING ON UBUNTU
+###
+
 ## Install vscode server
 curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
 tar -xf vscode_cli.tar.gz
@@ -19,16 +23,7 @@ sudo chmod -R 777 .
 
 ## Install dependencies
 sudo apt-get update
-Sudo apt-get install make
-Sudo apt-get install git -y
-
-## Install docker https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-debian-10
-## assuming on debian
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
+sudo apt-get install make
+sudo apt-get install git -y
+sudo apt-get install snap -y
+sudo snap install docker

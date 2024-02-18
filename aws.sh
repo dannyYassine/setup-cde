31eb7f48e3b699798d4abb88c4ee14a09008b1b0
Danny Yassine <dannyyassine@gmail.com>
2024-02-18 12:15:46 -0500

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -5,14 +5,14 @@ curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-
 tar -xf vscode_cli.tar.gz
 
 ## Run code server inline or background
-
 # Inline
 #./code tunnel
-
 # Background
 ./code tunnel service install
 # Monitor service
 ./code tunnel service log
+# Tip: run `sudo loginctl enable-linger $USER` to ensure the service stays running after you disconnect.
+sudo loginctl enable-linger $USER
 
 ## Set git info
 git config --global user.email “<your_email>”
@@ -23,10 +23,7 @@ sudo chmod -R 777 .
 
 ## Install dependencies
 sudo apt-get update
-Sudo apt-get install make
-Sudo apt-get install git -y
-
-## Install docker https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-debian-10
-## assuming on ubuntu
-sudo apt update
+sudo apt-get install make
+sudo apt-get install git -y
+sudo apt-get install snap -y
 sudo snap install docker
